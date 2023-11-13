cbfe9c982ff19f719d27903e7d3d23aa20b09b22
Pavel Zagalsky <zagalsky@gmail.com>
2023-11-13 16:04:45 +0200

    some fixes

--- app/app.py
+++ app/app.py
@@ -5,12 +5,6 @@ import sys
 
 app = Flask(__name__)
 
-# Define the log file path
-log_file = '/var/log/sample_app.log'
-
-# Check if the log file exists, and create it if it doesn't
-if not os.path.exists(log_file):
-    open(log_file, 'w').close()
 
 # Configure the logger to write to the log file and stdout
 logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
@@ -18,6 +12,17 @@ stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
 logging.getLogger().addHandler(stdout_handler)
 
+# Define the log file path
+log_file = '/var/log/sample_app.log'
+
+# Check if the log file exists, and create it if it doesn't
+try:
+    if not os.path.exists(log_file):
+        logging.info(f"{log_file} does not exist")
+        open(log_file, 'w').close()
+except Exception as e:
+    logging.error(f"There was a problem creating a file {e}")
+    
 @app.route('/')
 def hello_world():
     # Log "Hello, World!" to the log file and stdout
