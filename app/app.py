1ce8e3f941290786de38c1e7a8a7036112cacb48
Pavel Zagalsky <zagalsky@gmail.com>
2024-02-12 11:22:16 +0200

    some small fixes

--- app/app.py
+++ app/app.py
@@ -17,7 +17,8 @@ config = Config(
 jaeger_tracer = config.initialize_tracer()
 tracing = FlaskTracing(jaeger_tracer, True, app)
 
-log_file = '/var/log/sample_app.log'
+log_file = 'sample_app.log'
+# log_file = '/var/log/sample_app.log'
 
 # Configure the logger to write to the log file and stdout
 logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
@@ -40,6 +41,10 @@ def square(x):
     return f"The square of {x} is {x ** 2}"
 
 
+def cube(x):
+    return f"The cube of {x} is {x ** 4}"
+
+
 def calculate_fibonacci(n):
     fib_sequence = [0, 1]
     while len(fib_sequence) < n:
@@ -59,11 +64,22 @@ def call_square(num):
     return f"Calling square function: {result}"
 
 
+@app.route('/cube/<int:num>')
+def call_cube(num):
+    result = cube(num)
+    return f"Calling cube function: {result}"
+
+
 @app.route('/fibonacci/<int:n>')
 def generate_fibonacci(n):
     result = calculate_fibonacci(n)
     return jsonify({"fibonacci_sequence": result})
 
+@app.route('/my_name_is/<string:name>')
+def my_name_is(name):
+    if "_" in name:
+        names_list = name.split("_")
+    return f"My name is: {names_list[0].capitalize()} {names_list[1].capitalize()}"
 
 @app.route('/not_found')
 def not_found():
