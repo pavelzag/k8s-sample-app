9cef64720356549cb368b7ef21ed2567fbf08999
Pavel Zagalsky <zagalsky@gmail.com>
2024-02-15 20:43:25 +0200

    checking something else

--- app/app.py
+++ app/app.py
@@ -1,345 +1,141 @@
-
-from flask import Flask, jsonify
-from opentelemetry import trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-
-
-from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from flask import Flask, request, jsonify
+import time
+import signal
+import sys
 
 app = Flask(__name__)
 
-# Set up a TracerProvider
-trace.set_tracer_provider(TracerProvider())
-# Set the tracer provider
 
-# Create an OTLP span exporter
-otlp_exporter = OTLPSpanExporter(
-    # Endpoint of the Collector or Jaeger service accepting OTLP over gRPC
-    # Adjust the endpoint to your Jaeger gRPC endpoint if different
-    endpoint="my-release-jaeger-operator-metrics.istio-system:4317",  # Default OTLP gRPC endpoint
-    insecure=True,  # For demo purposes, use insecure connection
-)
+class InventoryItem:
+    def __init__(self, id, name, image):
+        self.id = id
+        self.name = name
+        self.image = image
 
 
-# Configure the Jaeger exporter to export traces to Jaeger
-# jaeger_exporter = JaegerExporter(
-#     agent_host_name='localhost',
-#     agent_port=6831,
-# )
-
-# Configure the tracer to use the Jaeger exporter
-trace.get_tracer_provider().add_span_processor(
-    BatchSpanProcessor(otlp_exporter)
-)
-
-# Instrument Flask app with OpenTelemetry middleware to trace requests
-FlaskInstrumentor().instrument_app(app)
-
-
-def square(x):
-    return f"The square of {x} is {x ** 2}"
-
-
-def cube(x):
-    return f"The cube of {x} is {x ** 4}"
-
-
-def calculate_fibonacci(n):
-    fib_sequence = [0, 1]
-    while len(fib_sequence) < n:
-        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-    return fib_sequence
+inventory_items = [
+    InventoryItem(1, "T Shirt", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f455.png"),
+    InventoryItem(2, "Pants", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f456.png"),
+    InventoryItem(3, "Shoes", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f462.png"),
+    InventoryItem(4, "Hat", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f9e2.png"),
+    InventoryItem(5, "Socks", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f9e6.png"),
+    InventoryItem(6, "Gloves", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f9e4.png"),
+    InventoryItem(7, "Scarf", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f9e3.png"),
+    InventoryItem(8, "Jacket", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f9e5.png"),
+    InventoryItem(9, "Kimono", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f458.png"),
+    InventoryItem(10, "Purse", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f45b.png"),
+    InventoryItem(11, "Tophat", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f3a9.png"),
+    InventoryItem(12, "Watch", "https://emoji.aranja.com/static/emoji-data/img-apple-160/231a.png"),
+    InventoryItem(13, "Sunglasses", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f576-fe0f.png"),
+    InventoryItem(14, "Womans Hat", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f452.png"),
+    InventoryItem(15, "Sandal", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f461.png"),
+    InventoryItem(16, "Bracelet", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f4ff.png"),
+    InventoryItem(17, "Ring", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f48d.png"),
+    InventoryItem(18, "Suit", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f454.png"),
+    InventoryItem(19, "Dress", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f457.png"),
+    InventoryItem(20, "Eyeglasses", "https://emoji.aranja.com/static/emoji-data/img-apple-160/1f453.png")
+]
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World from Flask with OpenTelemetry!'
+@app.route('/inventory', methods=['GET'])
+def get_inventory():
+    print("Returning inventory")
+    return jsonify([item.__dict__ for item in inventory_items])
 
 
-@app.route('/square/<int:num>')
-def call_square(num):
-    result = square(num)
-    return jsonify({"result": result})
+@app.route('/buy', methods=['POST'])
+def buy_product():
+    product_id = request.args.get('id', type=int)
+    print(f"Buying product with id {product_id}")
+    time.sleep(1)
+    return jsonify({"message": "Product purchased successfully"})
 
 
-@app.route('/cube/<int:num>')
-def call_cube(num):
-    result = cube(num)
-    return jsonify({"result": result})
-
-
-@app.route('/fibonacci/<int:n>')
-def generate_fibonacci(n):
-    result = calculate_fibonacci(n)
-    return jsonify({"fibonacci_sequence": result})
+def signal_handler(sig, frame):
+    print('Terminating inventory service')
+    sys.exit(0)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80)
+    signal.signal(signal.SIGTERM, signal_handler)
+    app.run(debug=False, port=8080, host='0.0.0.0')
 
 # from flask import Flask, jsonify
-# from jaeger_client import Config
-# from flask_opentracing import FlaskTracing
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import BatchSpanProcessor
+# from opentelemetry.instrumentation.flask import FlaskInstrumentor
+#
+#
+# from opentelemetry import trace
+# from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 #
 # app = Flask(__name__)
 #
-# # Jaeger configuration
-# config = Config(
-#     config={
-#         'sampler': {'type': 'const', 'param': 1},
-#         'logging': True,
-#         'reporter_batch_size': 1,
-#     },
-#     service_name="k8s-sample-app-service"
+# # Set up a TracerProvider
+# trace.set_tracer_provider(TracerProvider())
+#
+#
+# # Create an OTLP span exporter
+# otlp_exporter = OTLPSpanExporter(
+#     # Endpoint of the Collector or Jaeger service accepting OTLP over gRPC
+#     # Adjust the endpoint to your Jaeger gRPC endpoint if different
+#     endpoint="my-release-jaeger-operator-metrics.istio-system:4317",  # Default OTLP gRPC endpoint
+#     insecure=True,  # For demo purposes, use insecure connection
 # )
-# jaeger_tracer = config.initialize_tracer()
-# tracing = FlaskTracing(jaeger_tracer, True, app)
 #
 #
-# # def square(x):
-# #     with tracing.start_span('square') as span:
-# #         span.set_tag('number', x)
-# #         return f"The square of {x} is {x ** 2}"
-# #
-# #
-# # def cube(x):
-# #     with tracing.start_span('cube') as span:
-# #         span.set_tag('number', x)
-# #         return f"The cube of {x} is {x ** 4}"
-# #
-# #
-# # def calculate_fibonacci(n):
-# #     fib_sequence = [0, 1]
-# #     while len(fib_sequence) < n:
-# #         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-# #     return fib_sequence
-#
-#
-# @app.route('/hello')
-# def hello_world():
-#     with tracing.start_span('hello') as span:
-#         # Add tags or logs to the span as needed
-#         span.set_tag('result_length', 'something')
-#         return 'Hello from Flask!'
-#
-#
-# if __name__ == '__main__':
-#     # Run the Flask app on port 80 (requires root privileges)
-#     app.run(host='0.0.0.0', port=80)
-#
-# # @app.route('/square/<int:num>')
-# # def call_square(num):
-# #     with tracing.start_span('square') as span:
-# #         span.log_kv({'message': 'Hello from square endpoint'})
-# #         result = square(num)
-# #         return jsonify({"result": result})
-# #
-# #
-# # @app.route('/cube/<int:num>')
-# # def call_cube(num):
-# #     with tracing.start_span('cube') as span:
-# #         span.log_kv({'message': 'Hello from cube app!'})
-# #         result = cube(num)
-# #         return jsonify({"result": result})
-# #
-# #
-# # @app.route('/fibonacci/<int:n>')
-# # def generate_fibonacci(n):
-# #     with tracing.start_span('fibonacci') as span:
-# #         span.log_kv({'message': 'Hello from fibonacci endpoint!'})
-# #         result = calculate_fibonacci(n)
-# #         return jsonify({"fibonacci_sequence": result})
-#
-#
-# # if __name__ == '__main__':
-# #     # Run the Flask app on port 80 (requires root privileges)
-# #     app.run(host='0.0.0.0', port=80)
-#
-# # from flask import Flask, jsonify, abort
-# # import logging
-# # import os
-# # import sys
-# # from opentelemetry import trace
-# # from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-# # from opentelemetry.sdk.trace import TracerProvider
-# # from opentelemetry.sdk.trace.export import BatchSpanProcessor
-# #
-# # trace.set_tracer_provider(TracerProvider())
-# #
+# # Configure the Jaeger exporter to export traces to Jaeger
 # # jaeger_exporter = JaegerExporter(
 # #     agent_host_name='localhost',
 # #     agent_port=6831,
-# #     # optional: configure also collector
-# #     # collector_endpoint='http://localhost:14268/api/traces?format=jaeger.thrift',
-# #     # username=xxxx, # optional
-# #     # password=xxxx, # optional
-# #     # max_tag_value_length=None # optional
 # # )
-# #
-# # span_processor = BatchSpanProcessor(jaeger_exporter)
-# #
-# # trace.get_tracer_provider().add_span_processor(span_processor)
-# #
-# # tracer = trace.get_tracer(__name__)
-# #
-# # with tracer.start_as_current_span("foo"):
-# #     with tracer.start_as_current_span("bar"):
-# #         with tracer.start_as_current_span("baz"):
-# #             print("Hello world from OpenTelemetry Python!")
-# #
-# # stdout_handler = logging.StreamHandler(sys.stdout)
-# # stdout_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
-# # logging.getLogger().addHandler(stdout_handler)
-# #
-# # POD_NAME = os.getenv('HOSTNAME', 'default')
-# # K8S_SAMPLE_APP_SERVICE_SERVICE_HOST = os.getenv('K8S_SAMPLE_APP_SERVICE_SERVICE_HOST', 'default')
-# # app = Flask(__name__)
-# #
-# #
-# # def square(x):
-# #     return f"The square of {x} is {x ** 2}"
-# #
-# #
-# # def cube(x):
-# #     return f"The cube of {x} is {x ** 4}"
-# #
-# #
-# # def calculate_fibonacci(n):
-# #     fib_sequence = [0, 1]
-# #     while len(fib_sequence) < n:
-# #         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-# #     return fib_sequence
-# #
-# #
-# # @app.route('/hello')
-# # def hello_world():
-# #     with tracer.start_as_current_span("hello"):
-# #         logging.info(f"Hello from {POD_NAME}!")
-# #         return f"Hello from {POD_NAME} and {K8S_SAMPLE_APP_SERVICE_SERVICE_HOST}!"
-# #
-# #
-# # @app.route('/fibonacci/<int:n>')
-# # def generate_fibonacci(n):
-# #     with tracer.start_as_current_span("fibonacci"):
-# #         result = calculate_fibonacci(n)
-# #         return jsonify({"fibonacci_sequence": result})
-# #
-# #
-# # if __name__ == '__main__':
-# #     app.run(host='0.0.0.0', port=80)
 #
-# # from flask import Flask, jsonify, abort
-# # import logging
-# # import os
-# # import sys
-# # from jaeger_client import Config
-# # from flask_opentracing import FlaskTracing
-# # from opentelemetry import trace
-# # from opentelemetry.sdk.trace import TracerProvider
-# # from opentelemetry.sdk.trace.export import (
-# #     ConsoleSpanExporter,
-# #     SimpleExportSpanProcessor,
-# # )
-# # from opentelemetry.instrumentation.flask import FlaskInstrumentor
-# #
-# # trace.set_tracer_provider(TracerProvider())
-# # trace.get_tracer_provider().add_span_processor(
-# #     SimpleExportSpanProcessor(ConsoleSpanExporter())
-# # )
-# #
-# # app = Flask(__name__)
-# # FlaskInstrumentor().instrument_app(app)
-# #
-# # config = Config(
-# #     config={
-# #         'sampler':
-# #             {'type': 'const',
-# #              'param': 1},
-# #         'logging': True,
-# #         'reporter_batch_size': 1, },
-# #     service_name="k8s-sample-app-service")
-# # jaeger_tracer = config.initialize_tracer()
-# # tracing = FlaskTracing(jaeger_tracer, True, app)
-# #
-# # log_file = 'sample_app.log'
-# # # log_file = '/var/log/sample_app.log'
-# #
-# # # Configure the logger to write to the log file and stdout
-# # logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
-# # stdout_handler = logging.StreamHandler(sys.stdout)
-# # stdout_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
-# # logging.getLogger().addHandler(stdout_handler)
-# #
-# # POD_NAME = os.getenv('HOSTNAME', 'default')
-# # K8S_SAMPLE_APP_SERVICE_SERVICE_HOST = os.getenv('K8S_SAMPLE_APP_SERVICE_SERVICE_HOST', 'default')
-# #
-# # try:
-# #     if not os.path.exists(log_file):
-# #         logging.info(f"{log_file} does not exist")
-# #         open(log_file, 'w').close()
-# # except Exception as e:
-# #     logging.error(f"There was a problem creating a file {e}")
-# #
-# #
-# # def square(x):
-# #     return f"The square of {x} is {x ** 2}"
-# #
-# #
-# # def cube(x):
-# #     return f"The cube of {x} is {x ** 4}"
-# #
-# #
-# # def calculate_fibonacci(n):
-# #     fib_sequence = [0, 1]
-# #     while len(fib_sequence) < n:
-# #         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-# #     return fib_sequence
-# #
-# #
-# # @app.route('/hello')
-# # def hello_world():
-# #     logging.info(f"Hello from {POD_NAME}!")
-# #     return f"Hello from {POD_NAME} and {K8S_SAMPLE_APP_SERVICE_SERVICE_HOST}!"
-# #
-# #
-# # @app.route('/square/<int:num>')
-# # def call_square(num):
-# #     result = square(num)
-# #     return f"Calling square function: {result}"
-# #
-# #
-# # @app.route('/cube/<int:num>')
-# # def call_cube(num):
-# #     result = cube(num)
-# #     return f"Calling cube function: {result}"
-# #
-# #
-# # @app.route('/fibonacci/<int:n>')
-# # def generate_fibonacci(n):
-# #     result = calculate_fibonacci(n)
-# #     return jsonify({"fibonacci_sequence": result})
-# #
-# #
-# # @app.route('/my_name_is/<string:name>')
-# # def my_name_is(name):
-# #     if "_" in name:
-# #         names_list = name.split("_")
-# #     return f"My name is: {names_list[0].capitalize()} {names_list[1].capitalize()}"
-# #
-# #
-# # @app.route('/not_found')
-# # def not_found():
-# #     abort(404)
-# #
-# #
-# # @app.route('/internal_server_error')
-# # def internal_server_error():
-# #     abort(500)
-# #
-# #
-# # if __name__ == '__main__':
-# #     app.run(host='0.0.0.0', port=80)
+# # Configure the tracer to use the Jaeger exporter
+# trace.get_tracer_provider().add_span_processor(
+#     BatchSpanProcessor(otlp_exporter)
+# )
+#
+# # Instrument Flask app with OpenTelemetry middleware to trace requests
+# FlaskInstrumentor().instrument_app(app)
+#
+#
+# def square(x):
+#     return f"The square of {x} is {x ** 2}"
+#
+#
+# def cube(x):
+#     return f"The cube of {x} is {x ** 4}"
+#
+#
+# def calculate_fibonacci(n):
+#     fib_sequence = [0, 1]
+#     while len(fib_sequence) < n:
+#         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+#     return fib_sequence
+#
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World from Flask with OpenTelemetry!'
+#
+#
+# @app.route('/square/<int:num>')
+# def call_square(num):
+#     result = square(num)
+#     return jsonify({"result": result})
+#
+#
+# @app.route('/cube/<int:num>')
+# def call_cube(num):
+#     result = cube(num)
+#     return jsonify({"result": result})
+#
+#
+# @app.route('/fibonacci/<int:n>')
+# def generate_fibonacci(n):
+#     result = calculate_fibonacci(n)
+#     return jsonify({"fibonacci_sequence": result})
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True, port=80)
